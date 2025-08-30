ebc099d1ddd7f48e4b25f2a0431dfe86eb80a24d
aalexx59-png <aalexx59@gmail.com>
2025-08-31 01:28:49 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'v0_welcome.dart';
 import 'card_choice_page.dart';
-import 'solo_card_page.dart';
-import 'family_card_page.dart';
-import 'meal_history_section.dart';
 
 void main() {
   runApp(const ShakirDriveApp());
@@ -18,17 +15,13 @@ class ShakirDriveApp extends StatelessWidget {
       title: 'Shakir Drive',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,
-        scaffoldBackgroundColor: Colors.grey[100],
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.brown,
+        scaffoldBackgroundColor: Colors.orange.shade50,
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomePage(),
+        '/': (context) => const V0Welcome(),
         '/card-choice': (context) => const CardChoicePage(),
-        '/solo-card': (context) => const SoloCardPage(),
-        '/family-card': (context) => const FamilyCardPage(),
-        '/meal-history': (context) => const MealHistorySection(),
       },
     );
   }
