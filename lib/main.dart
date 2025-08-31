b25dec4f68bae7e272082c4dbcb882756069872d
aalexx59-png <aalexx59@gmail.com>
2025-08-31 05:40:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'v0_welcome.dart';
+import 'v1_home.dart';
 import 'card_choice_page.dart';
 
 void main() {
@@ -21,9 +21,34 @@ class ShakirDriveApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const V0Welcome(),
+        '/': (context) => const SplashScreen(), // nouvel écran temporaire
+        '/welcome': (context) => const V0Welcome(),
+        '/home': (context) => const V1Home(),
         '/card-choice': (context) => const CardChoicePage(),
       },
     );
+  }
+}
+
+/// SplashScreen qui redirige après 3 secondes
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/welcome');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const V0Welcome(); // affiche ton écran V0 pendant 3s
   }
 }
